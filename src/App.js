c59c03bd5842d9f9269a671f9dc6d7d3a7cf3e3a
mohamed amine <amineoueslati91@gmail.com>
2023-06-13 19:06:26 +0100

    creationWB

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+function App() {
+  return (
+    <div className="first">
+      <h1>document</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illo
+        ullam a perspiciatis repellat, voluptatibus ipsa odio modi,
+        necessitatibus fuga molestiae esse nam aspernatur eum nesciunt quisquam,
+        aliquam inventore ab?
+      </p>
+    </div>
+  );
+}
+export default App;
