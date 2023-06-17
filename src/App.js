d3ccff1b6d8653b3007b37275760444cc3e4698f
mohamed amine <amineoueslati91@gmail.com>
2023-06-17 11:44:22 +0100

    devlopment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { Navbar } from "react-bootstrap";
 function App() {
   return (
     <div className="first">
+      <nav className="header">
+        <a href="#welcome">welcome</a>
+        <a href="#contact">contact</a>
+      </nav>
       <h1>document</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illo
